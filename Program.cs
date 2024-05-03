f8d05e7d9164d63aa6bbf7018b2e89665f08ab18
Medelig <elen-kruk@mail.ru>
2024-05-03 16:08:24 +0300

    создали массив и метод для вывода массива

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿﻿System.Console.Write($"Введите значения через запятую в одну строчку для использования в массиве: ");
+
+string word = Console.ReadLine();
+
+string[] CreateArray()      // Создаем массив из введенный элементов.
+{
+    string[] arrayAll = word.Split(",");
+    return arrayAll;
+}
+
+void PrintArray(string[] array)  // Метод для вывода массива (Используется для финального массива.).
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        Console.Write(", ");
+    }
+}
